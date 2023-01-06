79b0bf01fe0bde58be18c4e7914f0d1bb2ac3c83
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-01-06 17:58:28 +0100

    sorsolás és gonext megy + results

--- TabbedPage/PlayPage.xaml.cs
+++ TabbedPage/PlayPage.xaml.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Text.Json;
 
 namespace FriendliOrAngri;
@@ -7,23 +7,24 @@ public class Software
 {
     public string Name { get; set; }
     public string Description { get; set; }
+    public bool IsFriendly { get; set; }
 }
 
 public partial class PlayPage : ContentPage
 {
-    public string RandomSoftware { get; set; }
-
+    public Software Software;
     public PlayPage()
     {
         InitializeComponent();
-        RandomSoftware = ChooseRandomSoftwareAsync().Result;
-        lbSoftware.Text = RandomSoftware;
+        ChooseRandomSoftwareAsync();
+
+
     }
-    public static async Task<string> ChooseRandomSoftwareAsync()
+    public async void ChooseRandomSoftwareAsync()
     {
         Random random = new();
-        bool isFriendli = random.Next(2) == 1;
-        string fileName = isFriendli ? "data_friendli.json" : "data_angri.json";
+        bool isFriendly = random.Next(2) == 1;
+        string fileName = isFriendly ? "data_friendli.json" : "data_angri.json";
         var stream = await FileSystem.OpenAppPackageFileAsync(fileName);
         string text = "";
         using (var reader = new StreamReader(stream))
@@ -32,6 +33,60 @@ public partial class PlayPage : ContentPage
         }
         List<Software> softwareList = JsonSerializer.Deserialize<List<Software>>(text);
         int randomIndex = random.Next(softwareList.Count);
-        return softwareList[randomIndex].Name;
+        Software = softwareList[randomIndex];
+        lbSoftware.Text = Software.Name;
+        Software.IsFriendly = isFriendly;
+    }
+
+    private void btnAngry_Clicked(object sender, EventArgs e)
+    {
+        ShowResult(false);
+    }
+
+    private void btnFriendly_Clicked(object sender, EventArgs e)
+    {
+        ShowResult(true);
+    }
+
+    private void ShowResult(bool isFriendly)
+    {
+        if (Software.IsFriendly == isFriendly)
+        {
+            lbResult.Text = "Correct!";
+        }
+        else
+        {
+            lbResult.Text = "Nope!";
+        }
+        if (Software.IsFriendly)
+        {
+            lbSoftware.TextColor = (Color)Application.Current.Resources.MergedDictionaries.ToList()[0]["FriendliColor"];
+            lbSoftware.Text = $"😇 {lbSoftware.Text}";
+        }
+        else
+        {
+            lbSoftware.TextColor = (Color)Application.Current.Resources.MergedDictionaries.ToList()[0]["AngriColor"];
+            lbSoftware.Text = $"😠 {lbSoftware.Text}";
+        }
+        lbDescription.Text = Software.Description;
+        btnNext.IsVisible = true;
+        btnAngry.IsEnabled = false;
+        btnFriendly.IsEnabled = false;
+        btnAngry.Opacity = 0.7;
+        btnFriendly.Opacity = 0.7;
+    }
+
+
+    private void btnNext_Clicked(object sender, EventArgs e)
+    {
+        lbDescription.Text = "";
+        lbResult.Text = "";
+        lbSoftware.TextColor = Colors.Black;
+        btnNext.IsVisible = false;
+        ChooseRandomSoftwareAsync();
+        btnAngry.IsEnabled = true;
+        btnFriendly.IsEnabled = true;
+        btnAngry.Opacity = 1;
+        btnFriendly.Opacity = 1;
     }
 }
