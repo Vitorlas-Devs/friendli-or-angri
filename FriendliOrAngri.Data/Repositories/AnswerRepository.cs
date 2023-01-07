f96feddcfb103e84d890857850186bdf0398fe0d
Menyhárt Attila <menyhart.atika@gmail.com>
2023-01-07 18:14:49 +0100

    AnswerRepository progress

--- FriendliOrAngri.Data/Repositories/AnswerRepository.cs
+++ FriendliOrAngri.Data/Repositories/AnswerRepository.cs
@@ -1,9 +1,29 @@
 ﻿using FriendliOrAngri.Data.Models;
 using FriendliOrAngriASP.Data.Repositories;
+using MongoDB.Driver;
 
 namespace FriendliOrAngri.Data.Repositories;
 
 public class AnswerRepository : GeneralRepository<AnswerModel>
 {
+    private IMongoCollection<AnswerModel> collection;
 
+    public AnswerRepository() =>
+        this.collection = this.database.GetCollection<AnswerModel>("answers");
+
+    public override IEnumerable<AnswerModel> GetAll()
+    {
+        List<AnswerModel> answers = this.collection.AsQueryable().ToList();
+        foreach (AnswerModel answer in answers)
+            yield return answer;
+    }
+
+    public override AnswerModel Insert(AnswerModel model)
+    {
+        model.Id = 1;
+        if (GetAll().Count() > 0)
+            model.Id = GetAll().Max(u => u.Id) + 1;
+        this.collection.InsertOne(model);
+        return model;
+    }
 }
