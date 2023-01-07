f7cd203b79d384013a9b76837090bb11ead85bb1
Menyhárt Attila <menyhart.atika@gmail.com>
2023-01-07 17:28:45 +0100

    UserRepository GetAll

--- FriendliOrAngri.Data/Repositories/UserRepository.cs
+++ FriendliOrAngri.Data/Repositories/UserRepository.cs
@@ -1,9 +1,25 @@
 ﻿using FriendliOrAngri.Data.Models;
 using FriendliOrAngriASP.Data.Repositories;
+using MongoDB.Driver;
 
 namespace FriendliOrAngri.Data.Repositories;
 
 public class UserRepository : GeneralRepository<UserModel>
 {
+    private IMongoCollection<UserModel> collection;
 
+    public UserRepository() =>
+        this.collection = this.database.GetCollection<UserModel>("users");
+
+    public override IEnumerable<UserModel> GetAll()
+    {
+        List<UserModel> users = this.collection.AsQueryable().ToList();
+        foreach (UserModel user in users)
+            yield return user;
+    }
+
+    public override UserModel Insert(UserModel item)
+    {
+        throw new NotImplementedException();
+    }
 }
