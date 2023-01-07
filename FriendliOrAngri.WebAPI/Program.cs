e267df31d9d187ea0741b1932cfd52ab54ffa609
Menyhárt Attila <menyhart.atika@gmail.com>
2023-01-07 19:06:48 +0100

    Átnevezések és megfelelő ASP.NET projekt

---
+++ FriendliOrAngri.WebAPI/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("EnableCors", policy =>
+    {
+        policy
+            .SetIsOriginAllowed(origin => true)
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials()
+            .Build();
+    });
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseCors("EnableCors");
+}
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
