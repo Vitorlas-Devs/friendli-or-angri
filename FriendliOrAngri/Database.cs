51f54d221abf131a899ac2cb39d069d78419e0dd
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-01-08 18:30:30 +0100

    idk delete these

---
+++ FriendliOrAngri/Database.cs
@@ -0,0 +1,56 @@
+﻿using FriendliOrAngri.WebAPI.Data.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendliOrAngri
+{
+    public class Database
+    {
+        SQLiteAsyncConnection database;
+
+        public Database()
+        {
+            database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+            database.CreateTableAsync<UserModel>().Wait();
+        }
+
+        public Task<List<UserModel>> GetItemsAsync()
+        {
+            return database.Table<UserModel>().ToListAsync();
+        }
+
+        public Task<UserModel> GetItemAsync(int id)
+        {
+            return database.Table<UserModel>().Where(i => i.Id == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveItemAsync(UserModel item)
+        {
+            if (item.Id != 0)
+            {
+                return database.UpdateAsync(item);
+            }
+            else
+            {
+                return database.InsertAsync(item);
+            }
+        }
+
+        public Task<int> DeleteItemAsync(UserModel item)
+        {
+            return database.DeleteAsync(item);
+        }
+
+        public Task<int> DeleteAllItemsAsync()
+        {
+            return database.DeleteAllAsync<UserModel>();
+        }
+
+        public Task<UserModel> GetUserByToken(string token) =>
+            database.Table<UserModel>().Where(u => u.Token == token).FirstOrDefaultAsync();
+    }
+}
