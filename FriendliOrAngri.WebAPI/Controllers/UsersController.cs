ead84981c95cb4c3fdea617d54979bc2b2fe254b
Menyhárt Attila <menyhart.atika@gmail.com>
2023-01-08 17:35:42 +0100

    GetUserByToken done

--- FriendliOrAngri.WebAPI/Controllers/UsersController.cs
+++ FriendliOrAngri.WebAPI/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using FriendliOrAngri.WebAPI.Data.Repositories;
+﻿using FriendliOrAngri.WebAPI.Data.Models;
+using FriendliOrAngri.WebAPI.Data.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FriendliOrAngri.WebAPI.Controllers
@@ -11,6 +12,16 @@ namespace FriendliOrAngri.WebAPI.Controllers
 
         public UsersController() =>
             this.userRepository = new();
+
+        [HttpGet]
+        public IActionResult GetUserByToken(string token) =>
+            this.Run(() =>
+            {
+                UserModel user = userRepository.GetUserByToken(token);
+                if (user == null)
+                    return BadRequest(404);
+                return Ok()
+            });
 
         [HttpPost]
         public IActionResult Insert(string userName) =>
