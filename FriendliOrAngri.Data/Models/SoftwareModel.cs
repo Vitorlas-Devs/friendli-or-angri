a11edc263e35f26832eabd721f623a40e0fd72b3
Menyhárt Attila <menyhart.atika@gmail.com>
2023-01-09 12:20:16 +0100

    Game- és SoftwareModel létrehozva

---
+++ FriendliOrAngri.Data/Models/SoftwareModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FriendliOrAngri.WebAPI.Data.Models;
+
+public class SoftwareModel
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public bool IsFriendli { get; set; }
+}
