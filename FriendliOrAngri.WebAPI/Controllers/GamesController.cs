c675edba42995fa88ae3cadb0be15d72b0619b54
Menyhárt Attila <menyhart.atika@gmail.com>
2023-01-09 21:42:41 +0100

    CreateNewGame request kész

---
+++ FriendliOrAngri.WebAPI/Controllers/GamesController.cs
@@ -0,0 +1,45 @@
+﻿using FriendliOrAngri.WebAPI.Data.Enums;
+using FriendliOrAngri.WebAPI.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendliOrAngri.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly GameRepository gameRepository;
+
+        public GamesController() =>
+            this.gameRepository = new();
+
+        [HttpPost]
+        public IActionResult CreateNewGame(string userToken, string gameMode) =>
+            this.Run(() =>
+            {
+                GameMode validGameMode;
+
+                switch (gameMode)
+                {
+                    case "normal":
+                        validGameMode = GameMode.Normal;
+                        break;
+                    case "hardcore":
+                        validGameMode = GameMode.Hardcore;
+                        break;
+                    default:
+                        throw new ArgumentException("Ismeretlen játékmód!");
+                }
+                
+                try
+                {
+                    this.gameRepository.CreateNewGame(userToken, validGameMode);
+                    return Ok();
+                }
+                catch (MissingMemberException e)
+                {
+                    return StatusCode(404, e.Message);
+                }
+            });
+    }
+}
